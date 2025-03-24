6f1a0f461250daa9d8d960a1226c92f71c3b9129
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-03-24 23:10:58 +0600

    improving

--- 1 - Js/index.js
+++ 1 - Js/index.js
@@ -13,3 +13,22 @@ let users = ["adnan", "oishi"];
 for (let i = 0; i < users.length; i++) {
   console.log(users[i]);
 }
+
+let persons = {
+  name: "Adnan",
+  age: 26,
+  gender: "male",
+};
+console.log(persons.name);
+console.log(persons["age"]);
+
+function greet(user) {
+  console.log("Hi " + user.name + ", your age is " + user.age);
+}
+
+let user = {
+  name: "Nur",
+  age: 27,
+};
+
+greet(user);
