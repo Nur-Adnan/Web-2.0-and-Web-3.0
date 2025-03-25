1e1833c4e3a1c4aef151985256ef8873ba63e58d
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-03-25 16:04:14 +0600

    passing a function as an argument

--- WEB 2.0/2 - Async JS/index.js
+++ WEB 2.0/2 - Async JS/index.js
@@ -1,4 +1,35 @@
 const fs = require("fs");
 
-const data = fs.readFileSync("a.txt", "utf-8");
+const content = fs.readFile("b.txt", "utf-8", function (err, content) {
+  console.log(content);
+}); // asynchronously
+
+const data = fs.readFileSync("a.txt", "utf-8"); // reading the file synchronously
 console.log(data);
+
+function sum(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+const ans = sub(10, 5);
+console.log(ans);
+
+// passing a function as an argument
+
+function doOperation(a, b, op) {
+  return op(a, b);
+}
+
+console.log(doOperation(15, 5, divide));
