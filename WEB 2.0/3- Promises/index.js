259ff719f894c95450c7c58ac555bcc48cc49226
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-03-26 16:37:18 +0600

    promise fully understood behide the sense

--- WEB 2.0/3- Promises/index.js
+++ WEB 2.0/3- Promises/index.js
@@ -40,32 +40,72 @@ map.set("age", 26);
 console.log(map.get("name"));
 
 // a promise class gives you a promise, that i will return you something in the future
-const fs = require("fs");
+/* 
+  const fs = require("fs");
 
-console.log("--- Top of the file ---");
+  console.log("--- Top of the file ---");
 
-// resolve arg..
-function readFile(value) {
-  console.log("Read Fill Called");
-  fs.readFile("a.txt", "utf-8", function (err, data) {
-    console.log("Callback based ReadFile Completed");
-    value(data);
-  });
-}
+  //* resolve arg..
+  function readFile(value) {
+    console.log("Read Fill Called");
+    fs.readFile("a.txt", "utf-8", function (err, data) {
+      console.log("Callback based ReadFile Completed");
+      value(data);
+    });
+  }
 
-// promise instance
-function readPromiseFile() {
-  console.log("readPromiseFile called");
-  return new Promise(readFile);
-  // first readfile call, after that readFile value means the argument called which somehow call the callback function.
-}
+  //* promise instance
+  function readPromiseFile() {
+    console.log("readPromiseFile called");
+    return new Promise(readFile);
+    //* first readfile call, after that readFile value means the argument called which somehow call the callback function.
+  }
 
-const p = readPromiseFile();
+  const p = readPromiseFile();
 
-function callback(contents) {
-  console.log("Reading the file completed");
-  console.log(contents);
-}
+  function callback(contents) {
+    console.log("Reading the file completed");
+    console.log(contents);
+  }
 
-p.then(callback);
-console.log("-- End of the File --");
+  p.then(callback);
+  console.log("-- End of the File --");
+*/
+
+// my inbuild promise
+
+/* 
+  class myPromise {
+    constructor(fn) {
+      //* new Promise(readFile), because it's take some function as an input
+      function afterDone() {
+        this.resolve();
+      }
+      fn(afterDone);
+    }
+    then(callback) {
+      this.resolve = callback;
+    }
+  }
+
+  function setTheTime(resolve) {
+    console.log("seTheTime called");
+    setTimeout(function () {
+      console.log("Callback based setTimeout completed");
+      resolve();
+    }, 3000);
+  }
+
+  function setTimeoutPromisified() {
+    return new myPromise(setTheTime);
+  }
+
+  let q = setTimeoutPromisified();
+
+  function myCallback() {
+    console.log("Callback has been called");
+  }
+  q.then(myCallback);
+
+*/
+
